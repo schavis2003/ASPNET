f10538143bf8d1c8f431ab1b7d08c08ee9625bf0
Shelia Chavis <schavis2003@gmail.com>
2023-08-08 17:21:41 -0400

    ASPNETMVC32 UPDATE Product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -21,6 +22,21 @@ namespace Testing.Controllers
         {
             var products = repo.GetAllProducts();
             return View(products);
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
     }
 }
